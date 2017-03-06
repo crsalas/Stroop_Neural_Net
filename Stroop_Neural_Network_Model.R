81ce11f15667fa1b9f6529a92f148ba6f1b0b6a6
Carlos Salas <carlos.r.salas@gmail.com>
2017-03-05 22:51:39 -0600

    Adds matrix for storing stimuli activation values in binary states

--- Stroop_Neural_Network_Model.R
+++ Stroop_Neural_Network_Model.R
@@ -12,3 +12,14 @@ setwd("C:/Users/Carlos Salas/Dropbox/1 Spring 2017/CompModels/Stroop_Neural_Net"
 # (Botvinick et al., 2001), and was structured to be the simplest model needed to 
 # in order to capture the Stroop task environment and instructional demands. 
 
+# Layer 1 = Input nodes. 
+# These will take on binary (0,1) states, and will accept a vector of 6 
+# values representing a potential stimulus. E.g., If a stimulus has the 
+# word "Red" printed in "Green" color ink, and the instruction is to 
+# name the color, then it would be specified as follows: 0,1,1,0,1,0,1,0
+
+# Matrix for storing stimuli activation values in binary states
+input <- data.frame(matrix(rep(NA,6),nrow =1,ncol =6))
+names(input) <- c("C.Red", "C.Green", "Task.Color","Task.Word",
+                  "W.Red", "W.Green")
+input    

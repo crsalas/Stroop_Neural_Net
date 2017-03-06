d7a835a0870dfccd10cb1f2a53eab15cacaaf319
Carlos Salas <carlos.r.salas@gmail.com>
2017-03-05 22:51:13 -0600

    Adds network structure definition

---
+++ Stroop_Neural_Network_Model.R
@@ -0,0 +1,14 @@
+# Author: Carlos Salas
+
+# Set working directory
+setwd("C:/Users/Carlos Salas/Dropbox/1 Spring 2017/CompModels/Stroop_Neural_Net")
+
+
+#####################
+# Network Structure #
+#####################
+# Note: All layers will initially be set up as node matrices with NAs as follows. The 
+# neural architecture was chosen based on previous models in the PDP literature 
+# (Botvinick et al., 2001), and was structured to be the simplest model needed to 
+# in order to capture the Stroop task environment and instructional demands. 
+

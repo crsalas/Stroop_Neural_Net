dfd4b9ec05d88bded675c97861de914888bd4ec5
Carlos Salas <carlos.r.salas@gmail.com>
2017-03-05 22:53:23 -0600

    Adds matrix for storing hidden node activation values

--- Stroop_Neural_Network_Model.R
+++ Stroop_Neural_Network_Model.R
@@ -19,7 +19,29 @@ setwd("C:/Users/Carlos Salas/Dropbox/1 Spring 2017/CompModels/Stroop_Neural_Net"
 # name the color, then it would be specified as follows: 0,1,1,0,1,0,1,0
 
 # Matrix for storing stimuli activation values in binary states
-input <- data.frame(matrix(rep(NA,6),nrow =1,ncol =6))
-names(input) <- c("C.Red", "C.Green", "Task.Color","Task.Word",
-                  "W.Red", "W.Green")
-input    
+    input <- data.frame(matrix(rep(NA,6),nrow =1,ncol =6))
+    names(input) <- c("C.Red", "C.Green", "Task.Color","Task.Word",
+                      "W.Red", "W.Green")
+    input    
+
+# Layer2  = Hidden Layer. 
+# This represents the intermediate nodes between the input and output layers. 
+# There are four nodes: two nodes in the Color pathway, and two in the 
+# Word pathway. Each node is linked and thus receives signals from three 
+# input nodes from layer 1. For hidden nodes in Color pathways, links feed in 
+# from both the Red and Green Color nodes + the Color Naming task node. For hidden 
+# nodes in the Word pathway, links feed in from both Red and Green Word 
+# nodes + the Word REading task node. The link weights are specified in list of 
+# weight matrices (see line 60).
+
+# Matrix for storing hidden node activation values
+    hidden <- data.frame(matrix((rep(NA,4)),1,4))
+    names(hidden) <- c("CH1", "CH2", "WH1", "WH2")
+    
+    hidden
+
+    
+    
+    
+    
+    

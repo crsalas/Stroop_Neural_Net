8ddf26ac9d639ce51e389061d6ef3c331f1ac461
Carlos Salas <carlos.r.salas@gmail.com>
2017-03-06 00:22:54 -0600

    Adds matrix for storing output node activation values

--- Stroop_Neural_Network_Model.R
+++ Stroop_Neural_Network_Model.R
@@ -13,31 +13,45 @@ setwd("C:/Users/Carlos Salas/Dropbox/1 Spring 2017/CompModels/Stroop_Neural_Net"
 # in order to capture the Stroop task environment and instructional demands. 
 
 # Layer 1 = Input nodes. 
-# These will take on binary (0,1) states, and will accept a vector of 6 
-# values representing a potential stimulus. E.g., If a stimulus has the 
-# word "Red" printed in "Green" color ink, and the instruction is to 
-# name the color, then it would be specified as follows: 0,1,1,0,1,0,1,0
+    # These will take on binary (0,1) states, and will accept a vector of 6 
+    # values representing a potential stimulus. E.g., If a stimulus has the 
+    # word "Red" printed in "Green" color ink, and the instruction is to 
+    # name the color, then it would be specified as follows: 0,1,1,0,1,0,1,0
 
-# Matrix for storing stimuli activation values in binary states
+    # Matrix for storing stimuli activation values in binary states
     input <- data.frame(matrix(rep(NA,6),nrow =1,ncol =6))
     names(input) <- c("C.Red", "C.Green", "Task.Color","Task.Word",
                       "W.Red", "W.Green")
-    input       
+    # input       
 
 # Layer2  = Hidden Layer. 
-# This represents the intermediate nodes between the input and output layers. 
-# There are four nodes: two nodes in the Color pathway, and two in the 
-# Word pathway. Each node is linked and thus receives signals from three 
-# input nodes from layer 1. For hidden nodes in Color pathways, links feed in 
-# from both the Red and Green Color nodes + the Color Naming task node. For hidden 
-# nodes in the Word pathway, links feed in from both Red and Green Word 
-# nodes + the Word REading task node. The link weights are specified in list of 
-# weight matrices (see line 60).
+    # This represents the intermediate nodes between the input and output layers. 
+    # There are four nodes: two nodes in the Color pathway, and two in the 
+    # Word pathway. Each node is linked and thus receives signals from three 
+    # input nodes from layer 1. For hidden nodes in Color pathways, links feed in 
+    # from both the Red and Green Color nodes + the Color Naming task node. For hidden 
+    # nodes in the Word pathway, links feed in from both Red and Green Word 
+    # nodes + the Word REading task node. The link weights are specified in list of 
+    # weight matrices.
 
-# Matrix for storing hidden node activation values
+    # Matrix for storing hidden node activation values
     hidden <- data.frame(matrix((rep(NA,4)),1,4))
     names(hidden) <- c("CH1", "CH2", "WH1", "WH2")
     
-    hidden
+    # hidden
 
+
+# Layer 3 = Output layer. 
+    # This represents the structure for the ouput layer. It has two nodes, one for 
+    # each color response (i.e., Red and Green). The activation of each of these 
+    # per cycle will be fed into a response mechanism (specified below). 
+    # The output nodes receive input from every node in the hidden layer.
+    
+    # Matrix for storing output node activation values
+    output <- data.frame(matrix(rep(NA,2), nrow =1, ncol = 2))
+    names(output) <- c("Red", "Green")
+    
+    # output    
+    
+    
     
